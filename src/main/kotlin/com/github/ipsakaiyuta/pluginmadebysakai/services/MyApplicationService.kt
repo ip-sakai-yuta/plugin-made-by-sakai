64cf8d2b50077faf91786a918c6d368cf1c493a9
GitHub Action <action@github.com>
2021-09-08 02:44:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ipsakaiyuta/pluginmadebysakai/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ipsakaiyuta.pluginmadebysakai.services
+
+import com.github.ipsakaiyuta.pluginmadebysakai.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
